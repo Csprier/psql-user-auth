29f3580ceb615e617886b83cfc083a5f2a3afd15
Cam Prier <CSPrier@gmail.com>
2019-10-29 16:55:25 -0400

    Added GET and GET/:id routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,3 +8,49 @@ CREATE TABLE users(
   last_login TIMESTAMP
 );
 */
+
+// =================================================================
+// Database
+import db from '../db/index';
+// Express & Router
+const express = require('express');
+const router = express.Router();
+
+// =================================================================
+// GET
+router.get('/', (req, res, next) => {
+  const getAllUsersQuery = 'SELECT * FROM users';
+  db.query(getAllUsersQuery, (err, res) => {
+    if (err) {
+      return next(err);
+    }
+    console.log(res.rows);
+    // res.json(res.rows);
+  });
+});
+
+// =================================================================
+// GET/:id
+router.get('/:id', (req, res, next) => {
+  const getUserByIDQuery = 'SELECT * FROM users WHERE id = $1';
+  const id = parseInt(req.params.id);
+  console.log('GET /:id', id);
+  db.query(getUserByIDQuery, [id], (err, res) => {
+    if (err) {
+      return next(err);
+    }
+    console.log(res.rows[0]);
+    // res.status(200).json(result.rows);
+  });
+})
+
+// =================================================================
+// POST
+
+// =================================================================
+// PUT
+
+// =================================================================
+// DELETE
+
+module.exports = router;
